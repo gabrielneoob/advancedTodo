097cd0b8400956d604c8a99d37148f58844cf9e0
Gabriel Neoob <gabrielngc@gmail.com>
2023-10-24 00:35:29 -0300

    feat: deleteTask and checkTask functions

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,13 +1,16 @@
 import axios from "axios"
 import { TodoTypes } from "../types/todo.types";
+import { useTodos } from "../context/todoContext";
 
 const api = axios.create({
   baseURL: "https://api-todolist-b8bf07f7bcd9.herokuapp.com/"
 })
 
 export const useApi = () => {
+  const { setUpdated } = useTodos();
   const getTodos = async ():Promise<TodoTypes[]> => {
     const data = await api.get('');
+    setUpdated(true);
     return data.data;
   }
 
@@ -17,12 +20,27 @@ export const useApi = () => {
     }, {
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}
     });
-    return data;
+    setUpdated(true);
+    return data.data;
+  }
+
+  const deleteTask = async(id: string) => {
+    const data = await api.delete(`/todo/${id}`);
+    setUpdated(true);
+    return data.data
+  }
+
+  const checkTask = async(id: string) => {
+    const data = await api.put(`/todo/check/${id}`);
+    setUpdated(true);
+    return data.data
   }
 
 
   return { 
     getTodos,
-    createTask
+    createTask,
+    deleteTask,
+    checkTask
   }
 }
