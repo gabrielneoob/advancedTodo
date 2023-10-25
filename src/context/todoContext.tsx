9d1b681a36cf94e8691807b65e76b286229cfc6d
Gabriel Neoob <gabrielngc@gmail.com>
2023-10-25 00:13:40 -0300

    feat: Context API

---
+++ src/context/todoContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { TodoTypes } from '../types/todo.types';
+import { useApi } from '../hooks/useApi';
+import { EditTodoType, TodoContextType } from '../types/TodoContext.type';
+
+const Context = createContext<TodoContextType>({
+  setUpdated: () => {},
+  setSearchTodos: () => {},
+  searchTodos: [],
+  updated: false,
+  editTodo: {
+    currentId: '',
+    edited: false,
+  },
+  setEditTodo: () => {},
+  selectInput: '',
+  setSelectInput: () => {},
+});
+
+
+export const ContextProvider = ({ children }: {children: React.ReactNode}) => {
+  const [ searchTodos, setSearchTodos ] = useState<TodoTypes[]>([]);
+  const [ selectInput, setSelectInput ] = useState<string>('all');
+  const [ editTodo, setEditTodo ] = useState<EditTodoType>({currentId: '', edited: false});
+  const [ updated, setUpdated ] = useState(false);
+  // const { getTodos } = useApi();
+
+  // useEffect(() => {
+  //   loadTodos()
+  // }, [])
+
+  // const loadTodos = async () => {
+  //   const data = await getTodos();
+  //   setTodos(data)
+  //   setUpdated(false)
+  // }
+  
+  // if(updated) {
+  //   loadTodos()
+  // }
+  return (
+    <Context.Provider value={{
+      setUpdated,
+      searchTodos,
+      setSearchTodos,
+      updated,
+      editTodo,
+      setEditTodo,
+      selectInput,
+      setSelectInput,
+    }}>
+      { children }
+    </Context.Provider>
+  )
+}
+
+export const useTodos = () => useContext(Context) 
